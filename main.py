41560d3b1b2266a2fb7f4f0de5c02040322b1de3
Timofey Prokhorov <prokhorovtd@yandex.ru>
2023-01-11 19:53:14 +0300

    HW updates

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from numpy import mean
 
+all_students = []
+all_lectures = []
+
 class Person:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
 
-
 class Student(Person):
+
     def __init__(self, name, surname, gender):
         super().__init__(name, surname)
         self.gender = gender
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        all_students.append(self)
+
 
     def get_average_grade(self):
         average_grade_per_course = str()
@@ -27,8 +32,8 @@ class Student(Person):
             courses += f'{i} '
         for i in self.finished_courses:
             finished_courses += f'{i} '
-        return f'Имя: {self.name} \nФамилия: {self.surname}\n{self.get_average_grade()}Курсы в процессе изучения: {courses}\n' \
-               f'Завершенные курсы: {finished_courses if False else "Еще не закончил, но все впереди"}'
+        return f'\nИмя: {self.name} \nФамилия: {self.surname}\n{self.get_average_grade()}Курсы в процессе изучения: {courses}\n' \
+               f'Завершенные курсы: {finished_courses if False else "Еще не закончил, но все впереди"}\n'
 
     def rate_mentor(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
@@ -50,16 +55,18 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
-
+        all_lectures.append(self)
     def get_average_grade(self):
-        average_grade_per_course = str()
+        average_grades = ''
         for key, value in self.grades.items():
-           average_grade_per_course += f'Средняя оценка по предмету {key}: {mean(value)}\n'
-        return average_grade_per_course
+            average_grades += f'Средняя оценка по предмету {key}: {mean(value)}\n'
+        return average_grades
+
 
 
     def __str__(self):
-        return f'Имя: {self.name} \nФамилия: {self.surname}\n{self.get_average_grade()}'
+        return f'\nИмя: {self.name} \nФамилия: {self.surname}\n{self.get_average_grade()}'
+
 
 
 class Reviewer(Mentor):
@@ -81,43 +88,71 @@ class Reviewer(Mentor):
 
 #Тесты
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-
-
 best_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['GIT']
 
 not_best_student = Student('Tim', 'Prokhorov', 'male')
+not_best_student.courses_in_progress += ['GIT']
+
 
 cool_reviewer = Reviewer('Some', 'Buddy')
-
-cool_lecturer = Lecturer('Mike', 'Davidson')
-
 cool_reviewer.courses_attached += ['Python']
 cool_reviewer.courses_attached += ['GIT']
-cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 7)
 cool_reviewer.rate_hw(best_student, 'GIT', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 2)
-cool_reviewer.rate_hw(best_student, 'GIT', 1)
-cool_reviewer.rate_hw(best_student, 'Python', 4)
-cool_reviewer.rate_hw(best_student, 'GIT', 5)
+cool_reviewer.rate_hw(not_best_student, 'Python', 8)
+cool_reviewer.rate_hw(not_best_student, 'GIT', 10)
+
+second_cool_reviewer = Reviewer('Buddy', 'Some')
+second_cool_reviewer.courses_attached += ['Python']
+second_cool_reviewer.rate_hw(best_student, 'Python', 7)
+second_cool_reviewer.rate_hw(best_student, 'GIT', 10)
+second_cool_reviewer.rate_hw(not_best_student, 'Python', 4)
+second_cool_reviewer.rate_hw(not_best_student, 'GIT', 10)
 
 
+cool_lecturer = Lecturer('Mike', 'Davidson')
 cool_lecturer.courses_attached += ['Python']
 cool_lecturer.courses_attached += ['GIT']
 
+second_cool_lecturer = Lecturer('Luca', 'Stricagnoli')
+second_cool_lecturer.courses_attached += ['Python']
 
-not_best_student.courses_in_progress += ['GIT']
+
 not_best_student.rate_mentor(cool_lecturer, 'GIT', 3)
 not_best_student.rate_mentor(cool_lecturer, 'GIT', 4)
+not_best_student.rate_mentor(second_cool_lecturer, 'GIT', 5)
+not_best_student.rate_mentor(second_cool_lecturer, 'Python', 4)
 
 best_student.rate_mentor(cool_lecturer, 'Python', 10)
 best_student.rate_mentor(cool_lecturer, 'Python', 9)
 best_student.rate_mentor(cool_lecturer, 'Python', 2)
+best_student.rate_mentor(second_cool_lecturer, 'Python', 10)
+best_student.rate_mentor(second_cool_lecturer, 'Git', 9)
+best_student.rate_mentor(second_cool_lecturer, 'Python', 2)
 
-print(best_student)
-print()
-print(cool_reviewer)
-print()
-print(cool_lecturer)
+
+def get_all_studs_grades(students, course='Python'):
+    res = []
+    for student in students:
+        if course in student.grades.keys():
+            for i in student.grades.values():
+                res += i
+            return f'Средняя отметка всех студентов по предмету {course}: {mean(res)}'
+    else:
+        return 'Еще никто не закончил'
+
+
+def get_all_lectures_grades(lectures, course='Python'):
+    res = []
+    for lectur in lectures:
+        if course in lectur.grades.keys():
+            for i in lectur.grades.values():
+                res += i
+            return f'Средняя отметка всех лекторов по предмету {course}: {mean(res)}'
+        else:
+            return 'Оценок еще нет'
+
+
 
 
